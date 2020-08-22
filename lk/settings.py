41bc4b1e62eb23d8c98c427caf99a69eb0dafd75
Mathis Beauville <mathis.beauville@gmail.com>
2020-08-22 02:27:42 +0200

    fix allow_host

--- lk/settings.py
+++ lk/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['lkshare.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['lkshare.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
