c039fa8f6f981f88f80d78843f964d54f477a515
Mathis Beauville <mathis.beauville@gmail.com>
2020-08-22 02:00:29 +0200

    Changing DDB to sqllite

--- lk/settings.py
+++ lk/settings.py
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'lk.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',   # Backends disponibles : 'postgresql', 'mysql', 'sqlite3' et 'oracle'.
-        'NAME': 'lk_bdd',
-        'USER': 'root',
-        'PASSWORD': '',        
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'sqlite3.db',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
