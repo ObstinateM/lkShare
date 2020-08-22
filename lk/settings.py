ade8a762811335826b8ef6ef98eaab41c0506ff2
Mathis Beauville <mathis.beauville@gmail.com>
2020-08-22 02:25:14 +0200

    debug = True

--- lk/settings.py
+++ lk/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = os.environ.get('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['lkshare.herokuapp.com/', '127.0.0.1']
 
